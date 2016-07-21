f25bb34ccf0f4cf01d5a6a56a983c9e3e5f1d96e
Ørjan Malde <foxyred333@gmail.com>
2016-07-21 21:50:12 +0200

    change #ifdef to proper macro

--- src/winclip.c
+++ src/winclip.c
@@ -533,7 +533,7 @@ win_paste(void)
   CloseClipboard();
 }
 
-#ifndef __midipix__
+#ifdef HOST_DRAG_DROP_DISABLED
 static volatile LONG dt_ref_count;
 
 static FORMATETC dt_format = { 0, null, DVASPECT_CONTENT, -1, TYMED_HGLOBAL };
